15a80890cf077301a55c3bb7d936d4af0f7e9c6d
Satya Katakamsetti (V) <Satya.Katakamsetti@shutterfly.com>
2025-08-15 13:11:49 +0530

    initial commit

---
+++ src/main/java/dev/learn/ai/workshop/chat/ChatController.java
@@ -0,0 +1,38 @@
+package dev.learn.ai.workshop.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.ChatClientResponse;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+public class ChatController {
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(){
+        return chatClient.prompt()
+                .user("Tell me an interesting fact about java")
+                .call().content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream(){
+        return chatClient.prompt()
+                .user("I'm visiting Hilton Head soon, can you give me 10 places I must visit?")
+                .stream()
+                .content();
+    }
+
+    @GetMapping("/joke")
+    public ChatClientResponse jok(){
+        return chatClient.prompt()
+                .user("tell me a joke!")
+                .call().chatClientResponse();
+    }
+}
